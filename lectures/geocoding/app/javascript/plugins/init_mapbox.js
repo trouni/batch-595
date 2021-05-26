516fba280c8cc3d070e326cb9c5f264af2eb992c
Trouni Tiet <trouni@gmail.com>
2021-05-26 10:42:01 +0900

    Complete geocoding lecture

---
+++ lectures/geocoding/app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,47 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3000 });
+  };
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/trouni/ckp4rsaio1k3d17nk59ixvt62'
+    });
+    
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }));
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'cover';
+      element.style.width = '30px';
+      element.style.height = '40px';
+
+      new mapboxgl.Marker(element)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+    });
+
+    fitMapToMarkers(map, markers)
+  }
+};
+
+export { initMapbox };
---
+++ lectures/geocoding/db/seeds.rb
@@ -0,0 +1,3 @@
+Flat.create(address: '16 Villa Gaudelet, Paris', name: 'Le Wagon HQ')
+Flat.create(address: "2-11-3 Meguro Tokyo 153-0063", name: "ImpactHUB Tokyo")
+Flat.create(address: "2-20-20 Meguro Tokyo 153-0064", name: "Origin Kitchen")
