0376f520e84a000da7bb74362a60dbb3d8f0bf76
Trouni Tiet <trouni@gmail.com>
2021-05-27 10:31:45 +0900

    Add PgSearch multisearch

--- lectures/imdb-search/app/controllers/movies_controller.rb
+++ lectures/imdb-search/app/controllers/movies_controller.rb
@@ -2,14 +2,22 @@ class MoviesController < ApplicationController
   def index
     @movies = 
       if params[:query]
-        sql_query = <<-SQL
-        movies.title @@ :q
-        OR movies.synopsis @@ :q
-        OR directors.first_name @@ :q
-        OR directors.last_name @@ :q
-        SQL
+        ## PG SEARCH
+        
+        # Movie.search_by_title_and_synopsis(params[:query])
+        # Movie.global_search(params[:query])
+        PgSearch.multisearch(params[:query])
 
-        Movie.joins(:director).where(sql_query, q: "%#{params[:query]}%")
+        ## ACTIVE RECORD
+
+        # sql_query = <<-SQL
+        # movies.title @@ :q
+        # OR movies.synopsis @@ :q
+        # OR directors.first_name @@ :q
+        # OR directors.last_name @@ :q
+        # SQL
+
+        # Movie.joins(:director).where(sql_query, q: "%#{params[:query]}%")
       else
         Movie.all
       end
