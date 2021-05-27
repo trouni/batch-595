a7cf59ccadf04c9c3caca8e3e7966afe3030989b
Trouni Tiet <trouni@gmail.com>
2021-05-27 09:37:30 +0900

    Implement ActiveRecord search

--- lectures/imdb-search/app/controllers/movies_controller.rb
+++ lectures/imdb-search/app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = 
+      if params[:query]
+        sql_query = <<-SQL
+        movies.title @@ :q
+        OR movies.synopsis @@ :q
+        OR directors.first_name @@ :q
+        OR directors.last_name @@ :q
+        SQL
+
+        Movie.joins(:director).where(sql_query, q: "%#{params[:query]}%")
+      else
+        Movie.all
+      end
   end
 end
