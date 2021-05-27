e67b8d0be4d80c95eaba5d7b27e4d4a5b9738d53
Trouni Tiet <trouni@gmail.com>
2021-05-27 23:13:47 +0900

    Fix migration

--- lectures/restaurants-ajaxified/db/migrate/20210527140354_create_restaurants.rb
+++ lectures/restaurants-ajaxified/db/migrate/20210527140354_create_restaurants.rb
@@ -2,6 +2,7 @@ class CreateRestaurants < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurants do |t|
       t.string :name
+      t.string :description
       t.string :address
 
       t.timestamps
